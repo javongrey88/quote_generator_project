fd68c041acdb0e77835f06f3701f0223654f5691
javongrey88 <33432540+javongrey88@users.noreply.github.com>
2021-01-16 14:23:35 -0600

    added images for quotes

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+const videoGameQuotes = [
+{quote: 'We all make choices in life, but in the end our choices make us.',
+character: 'Andrew Ryan',
+game :'Bioshock',
+year: 'August 21, 2007'
+},
+{quote: 'What is better? To be born good or to overcome your evil nature through great effort?',
+character: 'Paarthurnax',
+game :'Elders Scrolls V',
+year: 'November 11, 2011'
+},
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
